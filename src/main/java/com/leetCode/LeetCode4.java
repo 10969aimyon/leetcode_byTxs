5926f463d74b71f1c4165882e046a388a56a16ff
shuai <1129123733@qq.com>
2020-05-18 09:10:34 +0800

    LeetCode 4

---
+++ src/main/java/com/leetCode/LeetCode4.java
@@ -0,0 +1,28 @@
+package com.leetCode;
+
+/**
+ * 26. 删除排序数组中的重复项
+ */
+public class LeetCode4 {
+
+    public int removeDuplicates(int[] nums){
+        if(nums == null || nums.length == 0) return 0;
+        int q = 0;
+        int p = 1;
+        while (p < nums.length){
+            if (nums[q] == nums[p]){
+                p++;
+            }else {
+                nums[q+1] = nums[p];
+                q++;
+            }
+        }
+        return q+1;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
